d0b524fd894a2d0a21f1ef55672e3b743febaf01
emil <emil.salageanu@gmail.com>
2021-01-13 14:03:56 +0100

    Spring initialized

---
+++ src/test/java/com/esalagea/tutorial/jwt/JwtTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esalagea.tutorial.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
